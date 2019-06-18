a368dae4a3e6b9959f4602fd1ced515a9a71cdd6
Mario Bischoff <mariobischoffneto@gmail.com>
2019-06-17 22:57:33 -0300

    input password on login page

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,46 +1,67 @@
 <template>
   <q-layout>
     <q-page-container>
-      <q-page class="q-pa-md">
-        <div class="row justify-center">
-          <div class="col-xs-12 col-sm-8 col-md-6">
-            <q-card class="q-pa-lg">
-              <div class="row justify-center">
-                <q-card-section>
-                  <div class="text-center text-h5 ">Login</div>
-                </q-card-section>
-                <q-card-section class="q-gutter-md">
-                  <q-input
-                    label="Username"
-                    v-model="username"
-                    outlined
-                  />
-                  <q-input
-                    label="Password"
-                    v-model="password"
-                    outlined
-                  />
-                </q-card-section>
+      <q-page class="container">
+        <q-card class="card-login shadow-18">
 
-                <q-card-actions>
-                  <q-btn
-                    color="green"
-                  >
-                    Login
-                  </q-btn>
-                </q-card-actions>
-              </div>
-            </q-card>
-          </div>
-        </div>
+          <q-card-section>
+            <div class="text-center text-h5 ">Login</div>
+          </q-card-section>
+
+          <q-card-section class="q-gutter-md">
+            <q-input
+              label="Username"
+              v-model="username"
+              outlined
+              class="input"
+            >
+            </q-input>
+            <q-input
+              label="Password"
+              :type="showPw ? 'text' : 'password'"
+              v-model="password"
+              outlined
+              class="input"
+            >
+              <template v-slot:append>
+                <q-icon
+                  :name="showPw ? 'visibility' : 'visibility_off'"
+                  class="cursor-pointer"
+                  @click="showPw = !showPw"
+                />
+              </template>
+            </q-input>
+          </q-card-section>
+
+          <q-card-actions class="q-mb-lg">
+            <q-btn
+              color="green-7"
+              class="btn-login"
+            >
+              SIGN UP
+            </q-btn>
+          </q-card-actions>
+
+        </q-card>
       </q-page>
     </q-page-container>
   </q-layout>
 </template>
 
 <style lang="stylus">
-  .fundo
-    background-color green
+  .container
+    display flex
+    justify-content center
+    align-items center
+    background $light-blue-4
+    .card-login
+      display flex
+      flex-direction column
+      align-items center
+      .input
+        width 20em
+      .btn-login
+        width 10em
 </style>
 
 <script>
@@ -48,7 +69,8 @@ export default {
   data () {
     return {
       username: '',
-      password: ''
+      password: '',
+      showPw: false
     }
   }
 }
