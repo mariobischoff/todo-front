1687d43c2f811781380adeb8eb61dbf72b22e131
Mario Bischoff <mariobischoffneto@gmail.com>
2019-07-22 15:55:52 -0300

    fix

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,14 +2,7 @@ import axios from 'axios'
 import { SessionStorage } from 'quasar'
 
 const axiosInstance = axios.create({
-<<<<<<< HEAD
-  baseURL: 'http://localhost:3000',
-  headers: {
-    authorization: SessionStorage.getItem('token') || ''
-  }
-=======
   baseURL: 'https://stormy-springs-97309.herokuapp.com/'
->>>>>>> d8d45f1d550d25f7a7282560a98465f177d5d762
 })
 
 axiosInstance.interceptors.request.use(
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,10 +2,6 @@ import { SessionStorage } from 'quasar'
 
 const routes = [
   {
-<<<<<<< HEAD
-    path: '/auth',
-    component: () => import('layouts/AuthLayout.vue'),
-=======
     path: '/',
     component: () => import('layouts/Layout.vue'),
     beforeEnter: (to, from, next) => {
@@ -15,7 +11,6 @@ const routes = [
       }
       next('/auth/login')
     },
->>>>>>> d8d45f1d550d25f7a7282560a98465f177d5d762
     children: [
       { path: 'login', name: 'login', component: () => import('pages/Login') },
       { path: 'register', name: 'register', component: () => import('pages/Register') }
